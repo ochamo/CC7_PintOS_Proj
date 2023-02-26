9a1c8f9b267d9249ccd495100aa069bb3a0987af
ochamo <60203851+ochamo@users.noreply.github.com>
2023-02-26 20:56:18 +0000

    feat: get direction of waiters

--- threads/synch.c
+++ threads/synch.c
@@ -69,7 +69,7 @@ sema_down (struct semaphore *sema)
   // Tenemos que mantener la cola ordenada por prioridad
   while (sema->value == 0)
     {
-     list_insert_ordered(&sema->waiters, &thread_current()->elem, sort_by_greatest_priority, NULL);
+      list_insert_ordered(&sema->waiters, &thread_current()->elem, sort_by_greatest_priority, NULL);
       thread_block ();
     }
   sema->value--;
@@ -117,7 +117,7 @@ sema_up (struct semaphore *sema)
   sema->value++;
   if (!list_empty (&sema->waiters))
     // Nos aseguramos de que la lista este ordenada por prioridad
-    list_sort(&sema->waiters, sort_by_greatest_priority, NULL);
+    list_sort(&(sema->waiters), sort_by_greatest_priority, NULL);
 
     thread = list_entry(list_pop_front(&sema->waiters), struct thread, elem);
 
