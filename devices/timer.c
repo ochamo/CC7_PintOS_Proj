cd89212ad89eb1a85f6d41d0e667e912d2f96e70
marianvela <marian.vela@galileo.edu>
2023-04-19 09:13:07 +0000

    fix: interrupt handler mlfqs

--- devices/timer.c
+++ devices/timer.c
@@ -91,13 +91,15 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks)
 {
-  int64_t start = timer_ticks ();
+  //int64_t start = timer_ticks ();
 
-  ASSERT (intr_get_level () == INTR_ON);
+  if (ticks > 0)  {
+    ASSERT (intr_get_level () == INTR_ON);
   /*while (timer_elapsed (start) < ticks)
     thread_yield ();*/
 
-  insertar_en_lista_espera(ticks);
+    insertar_en_lista_espera(ticks);
+  }
 
 }
 
@@ -176,11 +178,10 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
 
-  enum intr_level old_level = intr_disable();
-
   ticks++;
   thread_tick ();
 
+  enum intr_level old_level = intr_disable();
   if(thread_mlfqs){
     thread_current()->recent_cpu = ADD_FIXED_REAL(thread_current()->recent_cpu, 1);
     if(ticks % TIMER_FREQ == 0){
@@ -191,11 +192,11 @@ timer_interrupt (struct intr_frame *args UNUSED)
       calculate_priority_mlfqs_all();
     }
   }
+  intr_set_level(old_level);
 
   // wake up threads
   remover_thread_durmiente(ticks);
 
-  intr_set_level(old_level);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
