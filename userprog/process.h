5abdc83bef5b49e7a66ce3457df0f51784093363
Abel <abel.alvarez@galileo.edu>
2023-04-21 00:04:20 +0000

    fix: :sparkles: process.h  restaurado

--- userprog/process.h
+++ userprog/process.h
@@ -1,48 +1,11 @@
 #ifndef USERPROG_PROCESS_H
 #define USERPROG_PROCESS_H
-#include <unistd.h>
 
 #include "threads/thread.h"
-#include "threads/synch.h"
 
-//definicion de pid_t
-typedef int pid_t;
-
-#define PID_ERROR  ((pid_t) -1)
-#define pid_init  ((pid_t) -2);
-
-pid_t process_execute (const char *file_name);
+tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
-
-
-struct process_control_block{
-  pid_t pid;
-
-  const char* file_name;
-
-  struct list_elem child_elem;
-  struct thread* parent_thread;
-
-  bool file_name_waiting;
-  bool file_name_exited;
-  bool file_name_orpahn;
-
-  int32_t exit_code;
-
-  struct semaphore sema_initialization;
-
-  struct semaphore sema_wait;
-
-};
-
-struct file_descriptor{
-  int id;
-  struct list_elem elem;
-  struct file* file;
-  struct dir* dir;
-
-}
 
 #endif /* userprog/process.h */
